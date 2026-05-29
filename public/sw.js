4b89faf8b9f83877d91c185b24742ab7f3992847
Z User <z@container>
2026-05-29 21:15:59 +0000

    526bba18-4775-465b-bacd-b91464e5f4cd

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,6 @@
-const CACHE_NAME = 'water-filling-v1'
-const urlsToCache = [
-  '/',
-]
+const CACHE_NAME = 'water-filling-v3'
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache)
-    })
-  )
   self.skipWaiting()
 })
 
@@ -16,9 +8,7 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames
-          .filter((name) => name !== CACHE_NAME)
-          .map((name) => caches.delete(name))
+        cacheNames.map((name) => caches.delete(name))
       )
     })
   )
@@ -26,25 +16,18 @@ self.addEventListener('activate', (event) => {
 })
 
 self.addEventListener('fetch', (event) => {
-  // Network-first strategy for API calls, cache-first for static assets
-  if (event.request.url.includes('/api/')) {
-    event.respondWith(
-      fetch(event.request).catch(() => caches.match(event.request))
-    )
-  } else {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request).then((fetchResponse) => {
-          // Cache new successful responses
-          if (fetchResponse.status === 200) {
-            const responseClone = fetchResponse.clone()
-            caches.open(CACHE_NAME).then((cache) => {
-              cache.put(event.request, responseClone)
-            })
-          }
-          return fetchResponse
-        })
+  // Network-first strategy for all requests
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response.status === 200) {
+          const responseClone = response.clone()
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseClone)
+          })
+        }
+        return response
       })
-    )
-  }
+      .catch(() => caches.match(event.request))
+  )
 })
