6191e0f11bff109ec285dbea47bdc68a69eaadcf
Z User <z@container>
2026-05-31 06:13:40 +0000

    153c50a6-2d4f-4cdb-8136-0d0a46cef27a

--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/z/my-project
 while true; do
-  node node_modules/.bin/next start -p 3000 -H 0.0.0.0
-  echo "Server crashed, restarting in 2 seconds..."
-  sleep 2
+  PORT=3000 HOSTNAME=0.0.0.0 node .next/standalone/server.js >> /home/z/my-project/dev.log 2>&1
+  echo "[$(date)] Server died, restarting in 3 seconds..." >> /home/z/my-project/dev.log
+  sleep 3
 done
